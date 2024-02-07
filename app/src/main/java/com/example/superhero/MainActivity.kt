dd5e907080107bec8dec19cacbf3f5e362eca000
BingPom <bingpom@mailfence.com>
2024-02-07 14:16:55 +0100

    Final Commit

--- app/src/main/java/com/example/superhero/MainActivity.kt
+++ app/src/main/java/com/example/superhero/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.superhero.repository.HeroesRepository.heroes
 import com.example.superhero.ui.theme.SuperheroTheme
 
